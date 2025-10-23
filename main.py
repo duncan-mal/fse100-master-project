69d1acc29aa7f09e1d22b5259e5c67510a896bfb
Duncan Maltman <140368237+duncan-mal@users.noreply.github.com>
2025-10-23 22:29:15 +0100

    Updated with GPIO support

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pyttsx3
 import time
 import sys
+import RPi.GPIO as GPIO
+from w1thermsensor import W1ThermSensor
+
+thermprobe = W1ThermSensor()
+
+GPIO.setmode(GPIO.BCM)
 
 tts = pyttsx3.init()
 tts.setProperty('rate', 150)
@@ -25,21 +31,23 @@ def ultrasonicreading():
 
 def read_temp():
     #Read Celsius temp from sensor (atm just using constant for testing)
-    temp_reading = 20
+    temp_reading = thermprobe.get_temperature()
 
     #reset vibration timer
     vibeTimer = 0
 
     #checking bounds for temp
-    if temp_reading < -10 | temp_reading > 120:
+    if temp_reading < -10 or temp_reading > 120:
         tts.say(" Temp out of range")
         return
 
     #Determing if reading should be Fahrenheit or Celsius
     if americanoMode:
         temp_reading = (temp_reading * (9/5)) +32
+        print(temp_reading)
         tts.say(str(temp_reading)+ " degrees fahrenheit")
     else:
+        print(temp_reading)
         tts.say( str(temp_reading)+ " degrees celsius")
 
     #Run TTS engine and wait to prevent attempt at talking over its self
@@ -63,12 +71,9 @@ def main():
     tts.say("Hello")
     tts.runAndWait()
 
-    ultraSonicReading()
-
     while True:
         #Reading from ultrasonic sensor (constant for testing)
-        ultraSonicReading = 10
-        if ultraSonicReading <= 25:
+        if ultrasonicreading():
             read_temp()
         else:
             time.sleep(1)
